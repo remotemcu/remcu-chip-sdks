f30866c253c72b260213580ffae0eae347a89630
Evgeny Chormonov <en.chormonov@gmail.com>
2019-03-06 02:58:25 +0300

    IMPR: link with remcu lib

--- stm8/STM8L15X/Libraries/STM8L15x_StdPeriph_Driver/inc/stm8l15x_conf.h
+++ stm8/STM8L15X/Libraries/STM8L15x_StdPeriph_Driver/inc/stm8l15x_conf.h
@@ -26,32 +26,32 @@
 #include "stm8l15x.h"
 
 /* Uncomment the line below to enable peripheral header file inclusion */
-/* #include "stm8l15x_adc.h" */
-/* #include "stm8l15x_beep.h" */
+#include "stm8l15x_adc.h"
+#include "stm8l15x_beep.h"
 #include "stm8l15x_clk.h"
-/* #include "stm8l15x_comp.h" */
-/* #include "stm8l15x_dac.h" */
-/* #include "stm8l15x_dma.h" */
-/* #include "stm8l15x_exti.h" */
-/* #include "stm8l15x_flash.h" */
-/* #include "stm8l15x_gpio.h" */
-/* #include "stm8l15x_i2c.h" */
-/* #include "stm8l15x_irtim.h" */
-/* #include "stm8l15x_itc.h" */
-/* #include "stm8l15x_iwdg.h" */
-/* #include "stm8l15x_lcd.h" */
-/* #include "stm8l15x_pwr.h" */
-/* #include "stm8l15x_rst.h" */
-/* #include "stm8l15x_rtc.h" */
-/* #include "stm8l15x_spi.h"*/
-/* #include "stm8l15x_syscfg.h" */
-/* #include "stm8l15x_tim1.h" */
-/* #include "stm8l15x_tim2.h" */
-/* #include "stm8l15x_tim3.h" */
-/* #include "stm8l15x_tim4.h" */
-/* #include "stm8l15x_usart.h" */
-/* #include "stm8l15x_wfe.h" */
-/* #include "stm8l15x_wwdg.h" */
+#include "stm8l15x_comp.h"
+#include "stm8l15x_dac.h"
+#include "stm8l15x_dma.h"
+#include "stm8l15x_exti.h"
+#include "stm8l15x_flash.h"
+#include "stm8l15x_gpio.h"
+#include "stm8l15x_i2c.h"
+#include "stm8l15x_irtim.h"
+#include "stm8l15x_itc.h"
+#include "stm8l15x_iwdg.h"
+#include "stm8l15x_lcd.h"
+#include "stm8l15x_pwr.h"
+#include "stm8l15x_rst.h"
+#include "stm8l15x_rtc.h"
+#include "stm8l15x_spi.h"
+#include "stm8l15x_syscfg.h"
+#include "stm8l15x_tim1.h"
+#include "stm8l15x_tim2.h"
+#include "stm8l15x_tim3.h"
+#include "stm8l15x_tim4.h"
+#include "stm8l15x_usart.h"
+#include "stm8l15x_wfe.h"
+#include "stm8l15x_wwdg.h"
 
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
---
+++ stm8/STM8L15X/STM8L15X_MD_FULL_EDU.cmake
@@ -0,0 +1,30 @@
+
+set(LICENSE_TYPE EDU)
+
+set(MCU_TYPE STM8L15X_MD)
+
+set(MCU_LIB_NAME STDPERIPH_DRIVER)
+
+set(MCU_MAJOR_VERSION_LIB V1.4.0)
+
+set(MCU_MINOR_VERSION_LIB 01)
+
+set(FULL_NAME_MCU_LIB ${MCU_TYPE}-${MCU_LIB_NAME}-${MCU_MAJOR_VERSION_LIB}-${MCU_MINOR_VERSION_LIB} )
+
+FILE(GLOB COPY_SOURCE_FROM_LIB "${CMAKE_CURRENT_LIST_DIR}/Libraries/STM8L15x_StdPeriph_Driver/inc/*.h")
+FILE(GLOB COPY_DESCRIPTION_FROM_LIB "${CMAKE_CURRENT_LIST_DIR}/README.txt")
+
+set(CONF_SOURCE ${CMAKE_CURRENT_LIST_DIR}/conf.cpp)
+
+set(MCU_OBJECT_FILE ${CMAKE_CURRENT_LIST_DIR}/build/${MCU_TYPE}.adin.o)
+
+add_custom_target( MCU_LIB
+	#OUTPUT ${MCU_OBJECT_FILE}
+                   COMMAND make all
+	                   REMCU_PATH_MK=${REMCU_PATH_MK}
+	                   OUTPUT=${MCU_OBJECT_FILE}
+	                   TARGET=${MCU_TYPE}
+                   CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+                   WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                   COMMENT "---------------- Generating ADIN IR ----------------"
+)
