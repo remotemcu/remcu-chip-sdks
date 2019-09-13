919187540092ed6ca4c2f204b5ecf6592ea1a017
Evgeny Chormonov <en.chormonov@gmail.com>
2019-09-13 13:39:51 +0300

    REFRACTORING: efm32tg840 example lcd print refractoring

--- SiLabs/Gecko_SDK/CMakeLists/EFM32TG840F32/test/lcd.c
+++ SiLabs/Gecko_SDK/CMakeLists/EFM32TG840F32/test/lcd.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "device_defines.h"
+#include "remcu.h"
 
 #include "em_device.h"
 #include "em_chip.h"
@@ -10,8 +10,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <assert.h>
-
-#include "remcu.h"
 
 /**************************************************************************//**
  * @brief  Main function
@@ -25,8 +23,8 @@ int main(int argc, char** argv)
         return -1;
     }
     const char * host = argv[1];
-    printf("argv[1] : %s\n", argv[1]);
-    printf("argv[2] : %s\n", argv[2]);
+    printf("server ip : %s\n", argv[1]);
+    printf("port string: %s\n", argv[2]);
     const uint16_t port = (atoi(argv[2]) & 0xFFFF);
     printf("port : %d\n", port);
 
@@ -36,9 +34,8 @@ int main(int argc, char** argv)
     remcu_connect2GDB(host, port, 3);
   }
 
-remcu_resetRemoteUnit(__HALT);
-//remcu_setVerboseLevel(LevelDebug::__INFO);
-remcu_setVerboseLevel(__ERROR);
+  //reset chip
+  remcu_resetRemoteUnit(__HALT);
 
   assert(remcu_is_connected());
   /* Chip errata */
@@ -52,20 +49,12 @@ remcu_setVerboseLevel(__ERROR);
 
   SegmentLCD_AllOff();
 
-  //SegmentLCD_Symbol(LCD_SYMBOL_PAD0, 1);
-  //SegmentLCD_Symbol(LCD_SYMBOL_PAD1, 1);
-
   sleep(1);
   SegmentLCD_Symbol(LCD_SYMBOL_GECKO, 1);
-      SegmentLCD_Symbol(LCD_SYMBOL_EFM32, 1);
+  SegmentLCD_Symbol(LCD_SYMBOL_EFM32, 1);
 
-  while(1){
-      sleep(1);
-      SegmentLCD_Write("REMCU");
 
-      sleep(1);
-      SegmentLCD_Write("     ");
-    }
+  SegmentLCD_Write("REMCU");
 
   /* Never going to reach this statement. Infinte loop in Test(). */
   return 0;
