c2e65d9550f5b57a5f6192523633add58b8aa830
Evgeny Chormonov <en.chormonov@gmail.com>
2019-10-12 07:10:52 +0300

    FIX: add STM8L15X_MD.py and STM32F10X_HD.py to root archive

--- stm8/STM8L15X/CMakeLists/STM8L15X_MD/2py.sh
+++ stm8/STM8L15X/CMakeLists/STM8L15X_MD/2py.sh
@@ -1,1 +1,1 @@
-python ../../../../parser/header2.py -c '-DSTM8L15X_MD -DUSE_STDPERIPH_DRIVER -DREMCU_LIB -w -I.' -a ../../Libraries/STM8L15x_StdPeriph_Driver/inc/ -o REMCU_STM8L15X_MD.py
+python ../../../../parser/header2.py -c '-DSTM8L15X_MD -DUSE_STDPERIPH_DRIVER -DREMCU_LIB -w -I.' -a ../../Libraries/STM8L15x_StdPeriph_Driver/inc/ -o STM8L15X_MD.py
