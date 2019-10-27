8c358c2f168d92db73b746023a606f0b1625b6b8
Evgeny Chormonov <en.chormonov@gmail.com>
2019-10-27 15:55:32 +0300

    REF: rename target STM32F4_DISCOVERY to STM32F4XX

--- stm32/stm32f4xx/STM32F4-Discovery_FW_V1.1.0/2py.sh
+++ stm32/stm32f4xx/STM32F4-Discovery_FW_V1.1.0/2py.sh
@@ -1,1 +1,1 @@
-python ../../../parser/header2.py -c '-DUSE_STDPERIPH_DRIVER -DREMCU_LIB -w -I. -ILibraries/inc -ILibraries/CMSIS/Include/' -a Libraries/STM32F4xx_StdPeriph_Driver/inc/ -a Libraries/CMSIS/ST/STM32F4xx/Include/ -o STM32F4XX_1.1.0.py
+python ../../../parser/header2.py -c '-DUSE_STDPERIPH_DRIVER -DREMCU_LIB -w -I. -ILibraries/inc -ILibraries/CMSIS/Include/' -a Libraries/STM32F4xx_StdPeriph_Driver/inc/ -a Libraries/CMSIS/ST/STM32F4xx/Include/ -o STM32F4XX_StdPeriph_Lib_V1_1_0.py
