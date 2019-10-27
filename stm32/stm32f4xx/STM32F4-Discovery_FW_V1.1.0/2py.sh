7980802e17ccfb23c8dc71dcbacfc8cb0eeaa90a
Evgeny Chormonov <en.chormonov@gmail.com>
2019-10-27 15:42:22 +0300

    REF: move STM32F4XX_1.1.0.py to remcu_include

--- stm32/stm32f4xx/STM32F4-Discovery_FW_V1.1.0/2py.sh
+++ stm32/stm32f4xx/STM32F4-Discovery_FW_V1.1.0/2py.sh
@@ -1,1 +1,1 @@
-python ../../../parser/header2.py -c '-DUSE_STDPERIPH_DRIVER -DREMCU_LIB -w -I. -ILibraries/inc -ILibraries/CMSIS/Include/' -a Libraries/STM32F4xx_StdPeriph_Driver/inc/ -a Libraries/CMSIS/ST/STM32F4xx/Include/ -o STM32F4_DISCOVERY.py
+python ../../../parser/header2.py -c '-DUSE_STDPERIPH_DRIVER -DREMCU_LIB -w -I. -ILibraries/inc -ILibraries/CMSIS/Include/' -a Libraries/STM32F4xx_StdPeriph_Driver/inc/ -a Libraries/CMSIS/ST/STM32F4xx/Include/ -o STM32F4XX_1.1.0.py
