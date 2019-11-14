c3b4da06dd35b10141c1269521f30d0a454ee10f
Evgeny Chormonov <en.chormonov@gmail.com>
2019-11-15 02:47:00 +0300

    IMPR: mark define with parameter(s) as comment in output file

--- parser/parserfile.py
+++ parser/parserfile.py
@@ -71,7 +71,7 @@ class ParseHeader(object):
 				continue
 			name = d[:first_space]
 			if name.find("(") >= 0:
-				#self.pytext.addComment("empty define %s" (d))
+				self.pytext.addComment("fun define %s" (d))
 				continue
 			self.pushValue(name)
 
