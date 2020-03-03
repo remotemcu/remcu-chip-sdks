f4054792704182535353122a9f7b653f4ccf68a3
Evgeny Chormonov <en.chormonov@gmail.com>
2020-03-03 04:02:22 +0300

    FIX: wrong build stm8 md. old build scripts

--- stm8/STM8L15X/CMakeLists/STM8L15X_MD/conf.cpp
+++ stm8/STM8L15X/CMakeLists/STM8L15X_MD/conf.cpp
@@ -1,13 +1,18 @@
 
-#include "TargetFun.h"
+#include "target.h"
 #include "AddressInterval.h"
 
 namespace remcu {
 
 void setConfig(){
     clearConfig();
-    add_to_mem_interval(0, 0x800); 			//SRAM
-	add_to_adin_interval(0x800,  0x10000); //Perephi
+    add_to_mem_interval(0, 0x400); 			//SRAM max 0x800 Bytes
+	add_to_adin_interval(0x5000,  0x5800); //Perephipheral registers
+	add_to_adin_interval(0x8000,  0x10000); //Flash
+}
+
+uint32_t get_RAM_addr_for_test(){
+    return 0;
 }
 
 } //namespace
