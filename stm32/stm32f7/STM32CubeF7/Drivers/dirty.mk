74ba201b6a0234eb3638627c77ae64bd494e3207
Evgeny Chormonov <en.chormonov@gmail.com>
2020-04-20 02:54:51 +0300

    WIP: build object files for F767 target

---
+++ stm32/stm32f7/STM32CubeF7/Drivers/dirty.mk
@@ -0,0 +1,30 @@
+CLANG			= clang-6.0
+CLANGPP 		= gcc++
+AR 				= llvm-ar
+COMPILE_FLAGS 	= -fPIC
+
+
+IR_FLAGS 			+= -g
+COMPILE_FLAGS 		+= -g
+
+BUILD_DIR 	= build
+OUTPUT = test
+
+CBE = echo
+
+IR_SUFFIX = ll
+
+OPT			= "echo"
+LD 			= "echo"
+
+all: clean_build $(BUILD_DIR) $(OUTPUT)
+
+
+.PHONY: clean_build
+clean_build:
+	rm -rf $(BUILD_DIR)
+
+$(BUILD_DIR):
+	mkdir -p $@
+
+print-%  : ; @echo $* = $($*)
