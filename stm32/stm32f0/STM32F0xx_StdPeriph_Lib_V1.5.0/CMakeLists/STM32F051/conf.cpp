4d1075b5a6d7001aa259b465691013cd0eef20e5
Evgeny Chormonov <en.chormonov@gmail.com>
2020-05-07 01:54:21 +0300

    FIX: old build CMakefiles.txt for STM32F051

--- stm32/stm32f0/STM32F0xx_StdPeriph_Lib_V1.5.0/CMakeLists/STM32F051/conf.cpp
+++ stm32/stm32f0/STM32F0xx_StdPeriph_Lib_V1.5.0/CMakeLists/STM32F051/conf.cpp
@@ -1,14 +1,19 @@
 
-#include "TargetFun.h"
+#include "target.h"
 #include "AddressInterval.h"
 
 namespace remcu {
 
 void setConfig(){
     clearConfig();
-    add_to_mem_interval(0x20000000, 0x20000000 + 0x1BFFF); 			//SRAM
-	add_to_adin_interval(0x40000000,  0x40000000 + 0x267FF); //Peripheral 
-	add_to_adin_interval(0xE0042000,  0xE0042000 + 0x267FF); //DBGMCU_BASE
+    add_to_mem_interval(0x20000000, 0x20000000 + 8*1024); 		//SRAM
+	add_to_adin_interval(0x40000000,  0x40000000 + 0x267FF); 	//Peripheral APB & AHB1
+	add_to_adin_interval(0x48000000,  0x40000000 + 0x17FF); 	//Peripheral AHB2
+	add_to_adin_interval(0xE0042000,  0xE0042000 + 0x267FF); 	//DBGMCU_BASE
+}
+
+uint32_t get_RAM_addr_for_test(){
+    return 0x20000000;
 }
 
 } //namespace
