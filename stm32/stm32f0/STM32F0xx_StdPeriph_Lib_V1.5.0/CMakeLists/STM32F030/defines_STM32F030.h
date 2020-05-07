01e3a97279fee3f89411eecc06d6f718b4dfbea2
Evgeny Chormonov <en.chormonov@gmail.com>
2020-05-07 14:39:00 +0300

    ADD: STM32F030 STM32F042 STM32F072 STM32F091

---
+++ stm32/stm32f0/STM32F0xx_StdPeriph_Lib_V1.5.0/CMakeLists/STM32F030/defines_STM32F030.h
@@ -0,0 +1,9 @@
+#define REMCU_LIB
+
+#define USE_STDPERIPH_DRIVER
+
+#define STM32F030
+
+//Memory region:
+#define START_AVAILABLE_MEMORY_REGION 0x20000000
+#define EMD_AVAILABLE_MEMORY_REGION (START_MEMORY_REGION + 8*1024) //first RAM bank of 8 KBytes
