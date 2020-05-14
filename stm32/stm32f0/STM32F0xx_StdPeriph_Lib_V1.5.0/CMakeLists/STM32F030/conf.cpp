38b1be66e6a361df347cc108660ae7f68cb0d356
Evgeny Chormonov <en.chormonov@gmail.com>
2020-05-14 19:43:01 +0300

    FIX:STM32F0: Error init virtual address : 0x48000000, 0x400017ff

--- stm32/stm32f0/STM32F0xx_StdPeriph_Lib_V1.5.0/CMakeLists/STM32F030/conf.cpp
+++ stm32/stm32f0/STM32F0xx_StdPeriph_Lib_V1.5.0/CMakeLists/STM32F030/conf.cpp
@@ -8,7 +8,7 @@ void setConfig(){
     clearConfig();
     add_to_mem_interval(0x20000000, 0x20000000 + 8*1024); 		//SRAM
 	add_to_adin_interval(0x40000000,  0x40000000 + 0x267FF); 	//Peripheral APB & AHB1
-	add_to_adin_interval(0x48000000,  0x40000000 + 0x17FF); 	//Peripheral AHB2
+	add_to_adin_interval(0x48000000,  0x48000000 + 0x17FF); 	//Peripheral AHB2
 	add_to_adin_interval(0xE0042000,  0xE0042000 + 0x267FF); 	//DBGMCU_BASE
 }
 
